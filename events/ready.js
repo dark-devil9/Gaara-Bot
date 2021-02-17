02bd0d0f85743d6aa9ccf6615c16f12b9777e7bf
dark-devil9 <64418950+dark-devil9@users.noreply.github.com>
2021-02-17 14:43:29 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[Gaara Music API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("*help | for commands", { // Change your bot's status here
+  await client.user.setActivity("*music | for commands", { // Change your bot's status here
     type: "PLAYING", //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
